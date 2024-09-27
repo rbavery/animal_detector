b1c15fa727d4c1614fd66a73773ca35a669edbde
Ryan Avery <ryanbarryavery@gmail.com>
2024-09-26 21:38:32 -0700

    broken docker build fix, it builds but cuda 11 required

--- mdv5app/docker_mdv5a_trt.sh
+++ mdv5app/docker_mdv5a_trt.sh
@@ -1,1 +1,1 @@
-docker run --gpus all -it -p 8080:8080 -p 8081:8081 -p 8082:8082 -v $1:/opt/ml/model torchserve-mdv5a-trt serve
+docker run --gpus all -it -p 8080:8080 -p 8081:8081 -p 8082:8082 -v $(pwd)/model_store_trt:/home/model-server/model-store torchserve-mdv5a
